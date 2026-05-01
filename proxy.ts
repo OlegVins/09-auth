192d6ec96fd415f78d7f3153cc51b12df22cc0a0
OlegVins <olegvinnickii@gmail.com>
2026-05-01 21:51:58 +0200

    fixed proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -3,19 +3,19 @@ import type { NextRequest } from "next/server";
 import { checkSession } from './lib/api/serverApi';
 
 
-export async function middleware(req: NextRequest) {
+export default async function proxy(req: NextRequest) {
     const pathname = req.nextUrl.pathname;
 
     const isAuthRoute = pathname.startsWith('/sign-in') ||
         pathname.startsWith('/sign-up');
-    
+
     const isPrivateRoute = pathname.startsWith('/notes') ||
         pathname.startsWith('/profile');
     const isAuthenticated = await checkSession();
-    
+
 
     if (!isAuthenticated && isPrivateRoute) {
-        return NextResponse.redirect(new URL('sign-in', req.url));
+        return NextResponse.redirect(new URL('/sign-in', req.url));
     }
 
     if (isAuthenticated && isAuthRoute) {
